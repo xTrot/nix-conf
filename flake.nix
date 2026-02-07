94cdc1a2e8f4d8a7b2c148eed6ef87bc41f0cae6
xTrot <enddyygf93@live.com>
2026-02-07 17:52:09 -0500

    Removing old commented out code.

--- flake.nix
+++ flake.nix
@@ -24,30 +24,5 @@
     flake-parts.lib.mkFlake {inherit inputs;}
     (import-tree ./modules);
 
-  # outputs = {
-  #   self,
-  #   nixpkgs,
-  #   ...
-  # } @ inputs: {
-  #   # use "nixos", or your hostname as the name of the configuration
-  #   # it's a better practice than "default" shown in the video
-  #
-  #   nixosConfigurations.xps13 = nixpkgs.lib.nixosSystem {
-  #     specialArgs = {inherit inputs;};
-  #     modules = [
-  #       ./host/xps13/configuration.nix
-  #       inputs.home-manager.nixosModules.default
-  #     ];
-  #   };
-  #
-  #   nixosConfigurations.performus = nixpkgs.lib.nixosSystem {
-  #     specialArgs = {inherit inputs;};
-  #     modules = [
-  #       ./host/performus/configuration.nix
-  #       inputs.home-manager.nixosModules.default
-  #     ];
-  #   };
-  # };
-
   # End old flake
 }
