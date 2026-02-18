5d0f39b2b10fa61f636db10f8a74dc4d79289329
xTrot <enddyygf93@live.com>
2026-02-18 12:56:03 -0500

    Fixing xps13 wallpapers.

--- modules/users/enddy/stow/home/dot-config/hypr/startup.sh
+++ modules/users/enddy/stow/home/dot-config/hypr/startup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 # Wallpaper
-swww init &
-swww img ~/Wallpapers/retroCity1.png &
+nohup sh swww-daemon &
+WALL_DIR="/home/enddy/Wallpaper"
+random_wallpaper="$(find $WALL_DIR -type f -print0 | shuf -z -n 1 | xargs -0 echo)"
+# echo "WALL_DIR: $WALL_DIR" > ~/chosen_wall.txt
+# echo "find: $(find $WALL_DIR -type f -print0)" >> ~/chosen_wall.txt
+# echo "shuf: $(find $WALL_DIR -type f -print0 | shuf -z -n 1)" >> ~/chosen_wall.txt
+echo "Chosen: $random_wallpaper" > ~/chosen_wall.txt
+sleep 1
+swww img $random_wallpaper
 
 # network
 nm-applet --indicator &
