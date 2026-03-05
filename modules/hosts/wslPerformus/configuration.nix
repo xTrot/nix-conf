7e75e86e89cc9d83d61dca150e519932c6da609e
xTrot <enddyygf93@live.com>
2026-03-05 06:22:52 -0500

    Fixing wsl defaultUser.

--- modules/hosts/wslPerformus/configuration.nix
+++ modules/hosts/wslPerformus/configuration.nix
@@ -23,6 +23,8 @@
     ];
 
     wsl.enable = true;
+    wsl.defaultUser = "enddy";
+
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
     networking.hostName = "wslPerformus"; # Define your hostname.
