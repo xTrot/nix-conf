7eb5bff3a20cfdc4aceb34d7e907bd4ebe603288
xTrot <enddyygf93@live.com>
2026-03-12 06:58:53 -0400

    Fixing ssh defaults warning message.

--- modules/users/enddy/homeManager.nix
+++ modules/users/enddy/homeManager.nix
@@ -158,6 +158,19 @@ in {
     services.ssh-agent.enable = true;
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       extraConfig = ''
         ControlMaster auto
         ControlPath ~/.ssh/controlmasters/%r@%h:%p
