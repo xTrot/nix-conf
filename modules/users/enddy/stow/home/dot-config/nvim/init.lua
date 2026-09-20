3536b1d9774d0bd6dbc986ee33a98085ef6708c7
xTrot <enddyygf93@live.com>
2026-09-20 18:01:50 -0400

    Removing paragragh jumping from the jump list in nvim.

--- modules/users/enddy/stow/home/dot-config/nvim/init.lua
+++ modules/users/enddy/stow/home/dot-config/nvim/init.lua
@@ -213,6 +213,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Do not add paragragh jumping to the jump list.
+vim.keymap.set('n', '{', ':keepjumps normal! {<CR>', { silent = true })
+vim.keymap.set('n', '}', ':keepjumps normal! }<CR>', { silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
